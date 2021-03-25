0474dd68b1f88a7127a7c720f0d094c79e270486
lorian-fate <ta01001011@gmail.com>
2021-03-25 12:07:23 +0100

    sd

--- V_ENV_VITO/main.py
+++ V_ENV_VITO/main.py
@@ -1,4 +1,12 @@
 import requests as req
 
-response = req.get("https://restcountries.eu/rest/v2/all")
+#response = req.get("https://restcountries.eu/rest/v2/all").json()
+#* Debe permitirnos buscar países por nombre y por continentes Cada uno de los países buscados debe quedar escrito en un archivo tipo csv que 
+#* solo admitira  los siguientes valores: name, capital, region, population, area, idioma (el primero), flag A su vez estos valores acturán como encabezados
+
+
+#name = input("Enter a country: ")
+#response = req.get(f"https://restcountries.eu/rest/v2/name/{name}").json()
+search_data = input("Enter country or continent: ")
+
 print(response)
